cda597969f421d99df7c415e27abb94b5bcb64aa
ioroioroi <kazuya.ioroi@google.com>
2017-10-22 17:57:46 +0900

    First Commit - atp-stat/court-bookerのheadless移植版です.

---
+++ config.rb
@@ -0,0 +1,8 @@
+@userid = 'xxxxxxxx'  # enter your account ID
+@passwd = 'yyyyyyyy'  # enter your account PW
+
+@rsrv_month = 'this_month'   # choices => 'this_month' or 'next_month'
+@rsrv_day = 'xx'    # ex. 11
+
+@rsrv_court_name = ['日比谷公園','猿江恩賜公園']  # ex. ['日比谷公園','芝公園','亀戸中央公園','木場公園']
+@rsrv_time = ['11:00','13:00','15:00','17:00'] # ex. ['11:00','13:00','9:00','17:00','19:00']
